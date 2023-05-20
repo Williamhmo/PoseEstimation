5f0ea86e0e9f1bbb34188ad7b63a40a3dea01d98
Hlaing Min Oo <52852283+Williamhmo@users.noreply.github.com>
2023-05-20 22:34:26 +0630

    Update app.py

--- app.py
+++ app.py
@@ -115,8 +115,7 @@ def uploadvideo():
                             cv2.putText(image,Action[num],(int(x)-50,int(y)-50), cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),2)
                         # output=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 
-                        imageLocation.image(image,width=400)
-                        st.write(c)
+                        imageLocation.image(image)
                     c+=1
         except:
             st.write('Success!')
